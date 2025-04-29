889e8376cd39a670dfa75bf71c8119dc1d6c51b8
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-04-29 12:08:14 +0900

    :sparkles:
    A기능
    
    C기능

--- week1Prac/src/test/test/Main.java
+++ week1Prac/src/test/test/Main.java
@@ -11,8 +11,8 @@ public class Main {
         System.out.println(a==b);
 
         HashMap<String, Integer> hash = new HashMap<>();
-        hash.put("one", 1233);
-        hash.put("onee", 1233);
+        hash.put("one", 12);
+        hash.put("onee", 12);
 
         System.out.println(hash.get("one") == hash.get("onee"));
     }
