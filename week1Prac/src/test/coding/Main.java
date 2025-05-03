4a578c809afd15a75b768ec4fd7742f9f6f42298
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-05-03 23:52:56 +0900

    Programmers 17

--- week1Prac/src/test/coding/Main.java
+++ week1Prac/src/test/coding/Main.java
@@ -3,19 +3,18 @@ package test.coding;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int ans = solution.solution(10);
+        int[] ans = solution.solution(12345);
         System.out.println(ans);
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; i < n; i++){
-            if ( n % i == 1)
-                answer = i;
-            break;
+    public int[] solution(long n) {
+        String a = Long.valueOf(n).toString();
+        int [] ans = new int[a.length()];
+        for (int i = 0; i < a.length(); i++) {
+            ans[i] = a.charAt(a.length() - i - 1)-'0';
         }
-        return answer;
+        return ans;
     }
 }
